00287afc4bf8e4c12bd96a8de4971fc52c9fb824
ef1301 <fangemily60@gmail.com>
2019-07-05 21:20:23 -0400

    first day of java

---
+++ java/helloworld.java
@@ -0,0 +1,17 @@
+//In java, every line of code that can actually run needs to be inside a class
+public class helloworld{ //declares public class called helloworld, which is the filename
+    public static void main(String[] args){
+	System.out.println("Hello World!");
+	/*
+	  System is a pre-defined class that java provides us and it holds some useful methods and variables
+out is a static variable with System that represents the output of your program(stdout
+println is a method of out that can be used to print a line
+	*/
+    }
+}
+/*
+public means anyone can access it
+static means that you can run this method without creating an instance of helloworld
+void doesn't return any value
+main is the name of the method
+*/
